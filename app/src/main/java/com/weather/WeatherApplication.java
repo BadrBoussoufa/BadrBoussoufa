6ca79d7085e6962897a6d9b172a4e9a65d0a498a
BadrBoussoufa <badr.boussoufa@revature.net>
2021-04-06 23:23:26 -0400

    wetherApp first code

--- app/src/main/java/com/weather/WeatherApplication.java
+++ app/src/main/java/com/weather/WeatherApplication.java
@@ -4,84 +4,60 @@ package com.weather;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
-//import java.util.ArrayList;
-//import java.util.HashMap;
-//import java.util.List;
-//import java.util.Map;
-//import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.List;
+import java.util.Map;
 
-//import com.weather.domain.User;
-//import com.weather.domain.WeatherInfo;
+import com.weather.domain.User;
+import com.weather.domain.WeatherInfo;
 
 /**
- * This program is about weather forcast according certain zipcodes.
+ * This program is about weather forcast according to certain zipcodes.
  * 
  * @author Badr Boussoufa
  * @since March 25, 2021
  */
 public class WeatherApplication {
-
-	
-
-	public static void main(String[] args) {
-		//Connect to DB first
-		String url = "jdbc:postgresql://localhost/weather";
-		String username = "BadrBoussoufa";
-		 String passw = "badr19811";
-
-		 try {
-			 
-		Connection connection = DriverManager.getConnection( url,username, passw);
-		Statement statement = connection.createStatement();
-		int info = statement.executeUpdate("insert into weatherlist_list Values(22099,'stormy','9')");
-		System.out.println(info);
-		ResultSet resultSet=statement.executeQuery("select * from WeatherList_list");
-		while(resultSet.next()){
-			System.out.println(resultSet.getInt("zipcode") + " " + resultSet.getString("forecast") + " " + resultSet.getString("temperature"));
-		}
-
-        Statement statement1 = connection.createStatement();
-		int users = statement.executeUpdate("insert into users_login Values('Ana','log3','pass3')");
-		System.out.println(users);
-		ResultSet resultSet1=statement1.executeQuery("select * from users_login");
-		while(resultSet1.next()){
-			System.out.println(resultSet1.getString("name") + " " + resultSet1.getString("username") + " " + resultSet1.getString("password"));
-		}
-
-      
-	    } catch (Exception sQLException) {
-		
-	    }
-	
-	}}
-        //Call to load users
-		//Call to load weathermap
-
-/*
+	public static void main(String[] args) throws Exception { 
+		Connection connection = getConnection();
+		List<User> users = loadUsers(connection);
+		Map<String, WeatherInfo> weatherMap = loadWeather(connection);
 
 		try (Scanner scanner = new Scanner(System.in)) {
             System.out.println("Enter your username : ");
             String username = scanner.nextLine();
-            System.out.println("Enter your password : ");
+            System.out.println("Enter your password : "); 
             String password = scanner.nextLine();
-            User loggedUser = findUser(userList, username, password);
+            User loggedUser = findUser(users, username, password);
 
             if (loggedUser != null) {
-                System.out.println("Enter your zipcode  : ");
-                String key = scanner.nextLine();
+				String answer;
+				do {
+					System.out.println("Enter your zipcode  : ");
+					String key = scanner.nextLine();
 
-                WeatherInfo info = weatherList.get(key);
-                if (info != null) {
-                    System.out.println(String.format("Hi %s, you weather is %s you temp %s", loggedUser.getName(), info.getForecast(), info.getTemperature()));
-                } else {
-                    System.out.println("Zipcode does not exist");
-                }
+					WeatherInfo info = weatherMap.get(key);
+					if (info != null) {
+						System.out.println(String.format("\n Hi %s, you weather is %s you temp %s\n", loggedUser.getName(), info.getForecast(), info.getTemperature()));
+					} else {
+						System.out.println("Zipcode does not exist");
+					}
+					System.out.println("Do you want to enter a new zipcode y/n");
+					answer = scanner.nextLine();
+			    } while ("y".equalsIgnoreCase(answer));
             } else {
                 System.out.println("Invalid username and password");
             }
         }
-    }
+	}
+        
+    private static Connection getConnection() throws SQLException { 
+	return DriverManager.getConnection("jdbc:postgresql://localhost/weather", "BadrBoussoufa", "badr19811");
+}
 
     private static User findUser(List<User> users, String username, String password) {
         User loggedUser = null;
@@ -93,42 +69,40 @@ public class WeatherApplication {
         return loggedUser;
     }
 
-    private static List<Users_login> loadUsers() {
+    private static List<User> loadUsers(Connection connection) throws SQLException {
         List<User> users = new ArrayList<>();
-        //Use the database connection to fetch users
-        //COnvert the list (for each) of rows to User object list
-        //return the list
+		ResultSet results = fetchUsers(connection);
+		while (results.next()) {
+			User user = new User(results.getString("username"), results.getString("password"), results.getString("name"));
+			users.add(user);
+		}
         return users;
     }
 
-    private static Map<String, WeatherInfo> loadWeather() {
+    private static Map<String, WeatherInfo> loadWeather(Connection connection) throws SQLException {
         Map<String, WeatherInfo> map = new HashMap<>();
-        //Use the database connection to fetch users
-        //COnvert the list (for each) of rows to Weather info object
-        // add the object to the map and use the weather info zip to be the key in the map
-        //return the list
+		ResultSet result = fetchWeatherInfo(connection);
+		while(result.next()){
+		WeatherInfo info = new WeatherInfo(result.getString("zipCode"), result.getString("forecast"), result.getInt("temperature"));
+		map.put(info.getZipCode(), info);
+		}
+        
         return map;
     
     }
 
+	private static ResultSet fetchUsers(Connection connection) throws SQLException {
+		return fetchData(connection, "select * from users_login");
+	}
 
+	private static ResultSet fetchWeatherInfo(Connection connection) throws SQLException {
+		return fetchData(connection, "select * from weatherlist_list");
+	}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-       }
-       }*/
+	private static ResultSet fetchData(Connection connection, String sqlStatement) throws SQLException {
+		 Statement statement = connection.createStatement() ;
+			return statement.executeQuery(sqlStatement);
+		
+			
+	}
+}
